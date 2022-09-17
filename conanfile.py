d4c890db15290adeb74b743df666b33e03d9e9ac
Dallas <nomalahvids@gmail.com>
2022-09-17 00:51:38 -0400

    Add config for pi4b and rename

--- conanfile.py
+++ conanfile.py
@@ -34,44 +34,28 @@ class GRCDash(ConanFile):
         "front_end_only": False
     }
 
-    generators = "CMakeDeps", "qt"
+    generators = "CMakeDeps"#, "qt"
     exports_sources = "CMakeLists.txt", "src/*"
 
     def validate(self):
         if self.settings.os == "Windows" and not self.options.front_end_only:
             raise ConanInvalidConfiguration("Windows backend for canbus not supported")
 
-    def setOptionsQT(self):
-        # Probably don't need most of these, but this is what was being used in the 
-        # original switch to conan.
-        self.options['qt'].shared=True
-        self.options['qt'].qtsvg=True
-        self.options['qt'].qtdeclarative=True
-        self.options['qt'].qtimageformats=True
-        self.options['qt'].qtmultimedia=True
-        self.options['qt'].qtgraphicaleffects=True
-        self.options['qt'].qtquickcontrols=True
-        self.options['qt'].qttools=True
-        self.options['qt'].qtwinextras=True
-        self.options['qt'].qtquickcontrols2=True
-
     def configure(self):
         if self.settings.compiler == 'Visual Studio':
             del self.options.fPIC
 
     def requirements(self):
-        self.requires("freetype/2.12.1") # Qt conflicts with PublicAPI
-        self.requires("openssl/1.1.1q") # Qt conflicts with CycloneDDS
-        self.requires("qt/5.15.5")
+        # self.requires("qt/6.3.1")
         self.requires("fmt/9.0.0")
-        self.setOptionsQT()
+        # self.setOptionsQT()
 
     def layout(self):
         cmake_layout(self)
     
     def generate(self):
         tc = CMakeToolchain(self)
-        tc.variables["QT_BIN_PATH"] = self.deps_cpp_info["qt"].bin_paths[0].replace("\\", "/")
+        #tc.variables["QT_BIN_PATH"] = self.deps_cpp_info["qt"].bin_paths[0].replace("\\", "/")
         tc.generate()
 
     def build(self):
