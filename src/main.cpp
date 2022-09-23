2db753068e5a74d0b384bda250c7b25678550635
Dallas Hart <nomalahvids@gmail.com>
2022-09-23 13:17:51 -0400

    Fixed RuntimeQml.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,23 @@
 #include <QApplication>
 #include <QQmlApplicationEngine>
-#include "RuntimeQml.hpp"
+#include <fmt/core.h>
+
+#ifdef QT_DEBUG
+    #include "RuntimeQml.hpp"
+#endif
 
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
     QQmlApplicationEngine engine;
-    RuntimeQml *rt = new RuntimeQml(&engine);
-    rt->load(QStringLiteral("qrc:/main.qml"));
-    rt->parseQrc("qml.qrc");
-    rt->setAutoReload(true);
+
+    #ifdef QT_DEBUG
+        RuntimeQml *rt = new RuntimeQml(&engine);
+        rt->parseQrc(ROOT_SOURCE_PATH "/qml.qrc");
+        rt->setAutoReload(true); // Reload automatically on file update
+
+        rt->load(QStringLiteral("qrc:/main.qml"));
+    #else
+        engine.load(QStringLiteral("qrc:/main.qml"));
+    #endif
     return app.exec();
 }
--- src/main.qml
+++ src/main.qml
@@ -1,9 +1,13 @@
-import QtQuick 2.15
-import QtQuick.Controls 2.15
+import QtQuick
+import QtQuick.Controls
 
 ApplicationWindow{
     visible: true
     width: 640
     height: 480
-    title: qsTr("Minimal Qml")
+    title: "GRC Dash"
+    Rectangle {
+        anchors.fill: parent
+        color: "Purple"
+    }
 }
